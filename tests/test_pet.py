5e3a2497d60ec3ad802f6f3c688650ed4c7f2154
viktrofimova <vktrfm10@gmail.com>
2026-08-27 12:57:30 +0300

    fix test: use correct pet data from task

--- tests/test_pet.py
+++ tests/test_pet.py
@@ -4,21 +4,8 @@ BASE_URL = "https://petstore.swagger.rv-school.ru/api/v3"
 
 def test_create_new_pet():
     request_body = {
-        "id": 10,
-        "name": "doggie",
-        "category": {
-            "id": 1,
-            "name": "Dogs"
-        },
-        "photoUrls": [
-            "string"
-        ],
-        "tags": [
-            {
-                "id": 0,
-                "name": "string"
-            }
-        ],
+        "id": 1,
+        "name": "Buddy",
         "status": "available"
     }
 
@@ -30,11 +17,6 @@ def test_create_new_pet():
     response_json = response.json()
 
     assert response.status_code == 200
-    assert response_json["id"] == 10
-    assert response_json["name"] == "doggie"
-    assert response_json["category"]["id"] == 1
-    assert response_json["category"]["name"] == "Dogs"
-    assert response_json["photoUrls"][0] == "string"
-    assert response_json["tags"][0]["id"] == 0
-    assert response_json["tags"][0]["name"] == "string"
+    assert response_json["id"] == 1
+    assert response_json["name"] == "Buddy"
     assert response_json["status"] == "available"
